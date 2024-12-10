29c975392f83f5d9f31f4a7cc1eb21499e011e12
Innokenty Dymbrylov <booterbrought@gmail.com>
2024-12-10 17:58:33 +0800

    Refactor WebSocket connection in GameService to support dynamic host and protocol based on environment

--- frontend/src/services/game.ts
+++ frontend/src/services/game.ts
@@ -98,7 +98,9 @@ export class GameService {
   }
 
   public wsConnect() {
-    this.ws = new WebSocket(`ws://localhost:8000/api/ws/${this.userStore.playerId}`);
+    const wsHost = import.meta.env.PROD ? window.location.host : 'localhost:8000';
+    const wsProtocol = import.meta.env.PROD ? 'wss' : 'ws';
+    this.ws = new WebSocket(`${wsProtocol}://${wsHost}/api/ws/${this.userStore.playerId}`);
     if (this.ws) {
       this.ws.onmessage = (event) => {
         this.gameState.value = JSON.parse(event.data).game;
